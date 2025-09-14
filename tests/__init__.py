7caf83cecbf0f5a73e66d393a7bd39b30b27ca5c
pmpmt <pmpmtj@hotmail.com>
2025-09-14 13:26:24 +0100

    change filename

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Test package for the YouTube downloader application.
-
-This package contains all unit tests, integration tests, and test utilities
-for the multiuser YouTube downloader system.
-"""
